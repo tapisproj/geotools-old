geotools/geotools

GEOT-4229 fix join of more than two tables
Signed-off-by: r0bb3n <r0bb3n@gmx.de>

modules/library/jdbc/src/test/java/org/geotools/jdbc/JDBCJoinTest.java
@@ -17,7 +17,9 @@
 package org.geotools.jdbc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.geotools.data.Join;
@@ -109,7 +111,7 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
             assertEquals("two", f.getAttribute(aname("stringProperty")));
             
             SimpleFeature g = (SimpleFeature) f.getAttribute(aname("ftjoin"));
-            assertEquals(3, g.getAttributeCount());
+            assertEquals(4, g.getAttributeCount());
             assertEquals(2, ((Number)g.getAttribute(aname("id"))).intValue());
             assertEquals("two", g.getAttribute(aname("name")));
         }
@@ -417,7 +419,7 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
             
             while(it.hasNext()) {
                 SimpleFeature f = it.next();
-                assertEquals(4, f.getAttributeCount());
+                assertEquals(5, f.getAttributeCount());
                 
                 SimpleFeature g = (SimpleFeature) f.getAttribute(tname("ft1"));
                 if ("three".equals(f.getAttribute(aname("name")))) {
@@ -426,6 +428,47 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
                 else {
                     assertNotNull(g);
                 }
+            }
+        }
+        finally {
+            it.close();
+        }
+    }
+
+    public void testJoinMoreThanTwo() throws Exception {
+    doJoinMoreThanTwo(false);
+    doJoinMoreThanTwo(true);
+    }
+    
+    void doJoinMoreThanTwo(boolean exposePrimaryKeys) throws Exception {
+        dataStore.setExposePrimaryKeyColumns(exposePrimaryKeys);
+        FilterFactory ff = dataStore.getFilterFactory();
+        Query q = new Query(tname("ftjoin"));
+        q.getJoins().add(new Join(tname("ft1"), 
+            ff.equal(ff.property(aname("name")), ff.property(aname("stringProperty")), true)));
+
+        q.getJoins().add(new Join(tname("ftjoin2"), 
+                ff.equal(ff.property(aname("join2intProperty")), ff.property(aname("join1intProperty")), true)));
+
+        SimpleFeatureCollection features = dataStore.getFeatureSource(tname("ftjoin")).getFeatures(q);
+        assertEquals(3, features.size());
+
+        SimpleFeatureIterator it = features.features();
+        try {
+            String[] ft1StringProp = new String[]{"zero","one","two"};
+            String[] ftjoin2StringProp = new String[]{"2nd zero","2nd one","2nd two"};
+            
+            while(it.hasNext()) {
+                SimpleFeature f = it.next();
+                assertEquals(6, f.getAttributeCount());
+                Integer idx = (Integer) f.getAttribute(aname("join1intProperty"));
+                assertTrue(idx < 3);
+                SimpleFeature g = (SimpleFeature) f.getAttribute(tname("ft1"));
+                assertNotNull(g);
+                assertEquals(ft1StringProp[idx], g.getAttribute(aname("stringProperty")));
+                g = (SimpleFeature) f.getAttribute(tname("ftjoin2"));
+                assertNotNull(g);
+                assertEquals(ftjoin2StringProp[idx], g.getAttribute(aname("stringProperty2")));
             }
         }
         finally {

modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisJoinTestSetup.java
@@ -31,18 +31,26 @@ public class PostgisJoinTestSetup extends JDBCJoinTestSetup {
 
     @Override
     protected void createJoinTable() throws Exception {
-        run("CREATE TABLE \"ftjoin\" ( \"id\" int, " + "\"name\" VARCHAR, \"geom\" GEOMETRY)" );
+        run("CREATE TABLE \"ftjoin\" ( \"id\" int, " + "\"name\" VARCHAR, \"geom\" GEOMETRY, \"join1intProperty\" int)" );
         run("INSERT INTO geometry_columns VALUES ('', 'public', 'ftjoin', 'geom', 2, 4326, 'GEOMETRY')");
         
-        run( "INSERT INTO \"ftjoin\" VALUES (0, 'zero', ST_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (1, 'one', ST_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (2, 'two', ST_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (3, 'three', NULL)");
+        run( "INSERT INTO \"ftjoin\" VALUES (0, 'zero', ST_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326), 0)");
+        run( "INSERT INTO \"ftjoin\" VALUES (1, 'one', ST_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326), 1)");
+        run( "INSERT INTO \"ftjoin\" VALUES (2, 'two', ST_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326), 2)");
+        run( "INSERT INTO \"ftjoin\" VALUES (3, 'three', NULL, 3)");
+        
+        run( "CREATE TABLE \"ftjoin2\"(\"id\" int, \"join2intProperty\" int, \"stringProperty2\" varchar)");
+        run( "INSERT INTO \"ftjoin2\" VALUES (0, 0, '2nd zero')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (1, 1, '2nd one')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (2, 2, '2nd two')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (3, 3, '2nd three')");
+        
     }
 
     @Override
     protected void dropJoinTable() throws Exception {
         run( "DROP TABLE \"ftjoin\"" );
+        run( "DROP TABLE \"ftjoin2\"" );
         run( "DELETE FROM geometry_columns WHERE f_table_name = 'ftjoin'");
     }
 

tapisproj/geotools-old

modules/library/jdbc/src/test/java/org/geotools/jdbc/JDBCJoinTest.java
@@ -17,7 +17,9 @@
 package org.geotools.jdbc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.geotools.data.Join;
@@ -109,7 +111,7 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
             assertEquals("two", f.getAttribute(aname("stringProperty")));
             
             SimpleFeature g = (SimpleFeature) f.getAttribute(aname("ftjoin"));
-            assertEquals(3, g.getAttributeCount());
+            assertEquals(4, g.getAttributeCount());
             assertEquals(2, ((Number)g.getAttribute(aname("id"))).intValue());
             assertEquals("two", g.getAttribute(aname("name")));
         }
@@ -417,7 +419,7 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
             
             while(it.hasNext()) {
                 SimpleFeature f = it.next();
-                assertEquals(4, f.getAttributeCount());
+                assertEquals(5, f.getAttributeCount());
                 
                 SimpleFeature g = (SimpleFeature) f.getAttribute(tname("ft1"));
                 if ("three".equals(f.getAttribute(aname("name")))) {
@@ -426,6 +428,48 @@ public abstract class JDBCJoinTest extends JDBCTestSupport {
                 else {
                     assertNotNull(g);
                 }
+            }
+        }
+        finally {
+            it.close();
+        }
+    }
+
+    public void testJoinMoreThanTwo() throws Exception {
+    doJoinMoreThanTwo(false);
+    doJoinMoreThanTwo(true);
+    }
+    
+    void doJoinMoreThanTwo(boolean exposePrimaryKeys) throws Exception {
+        dataStore.setExposePrimaryKeyColumns(exposePrimaryKeys);
+        FilterFactory ff = dataStore.getFilterFactory();
+        Query q = new Query(tname("ftjoin"));
+        q.getJoins().add(new Join(tname("ft1"), 
+            ff.equal(ff.property(aname("name")), ff.property(aname("stringProperty")), true)));
+
+        q.getJoins().add(new Join(tname("ftjoin2"), 
+                ff.equal(ff.property(aname("join2intProperty")), ff.property(aname("join1intProperty")), true)));
+
+        SimpleFeatureCollection features = dataStore.getFeatureSource(tname("ftjoin")).getFeatures(q);
+        assertEquals(3, features.size());
+
+        SimpleFeatureIterator it = features.features();
+        try {
+            String[] ft1StringProp = new String[]{"zero","one","two"};
+            String[] ftjoin2StringProp = new String[]{"2nd zero","2nd one","2nd two"};
+            
+            while(it.hasNext()) {
+                SimpleFeature f = it.next();
+                assertEquals(6, f.getAttributeCount());
+                Number nmb = (Number) f.getAttribute(aname("join1intProperty"));
+                Integer idx = nmb.intValue(); 
+                assertTrue(idx < 3);
+                SimpleFeature g = (SimpleFeature) f.getAttribute(tname("ft1"));
+                assertNotNull(g);
+                assertEquals(ft1StringProp[idx], g.getAttribute(aname("stringProperty")));
+                g = (SimpleFeature) f.getAttribute(tname("ftjoin2"));
+                assertNotNull(g);
+                assertEquals(ftjoin2StringProp[idx], g.getAttribute(aname("stringProperty2")));
             }
         }
         finally {

modules/plugin/jdbc/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisJoinTestSetup.java
@@ -31,19 +31,27 @@ public class PostgisJoinTestSetup extends JDBCJoinTestSetup {
 
     @Override
     protected void createJoinTable() throws Exception {
-        run("CREATE TABLE \"ftjoin\" ( \"id\" int, " + "\"name\" VARCHAR, \"geom\" GEOMETRY)" );
+        run("CREATE TABLE \"ftjoin\" ( \"id\" int, " + "\"name\" VARCHAR, \"geom\" GEOMETRY, \"join1intProperty\" int)" );
         run("INSERT INTO geometry_columns VALUES ('', 'public', 'ftjoin', 'geom', 2, 4326, 'GEOMETRY')");
         
-        run( "INSERT INTO \"ftjoin\" VALUES (0, 'zero', ST_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (1, 'one', ST_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (2, 'two', ST_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326))");
-        run( "INSERT INTO \"ftjoin\" VALUES (3, 'three', NULL)");
+        run( "INSERT INTO \"ftjoin\" VALUES (0, 'zero', ST_GeomFromText('POLYGON ((-0.1 -0.1, -0.1 0.1, 0.1 0.1, 0.1 -0.1, -0.1 -0.1))', 4326), 0)");
+        run( "INSERT INTO \"ftjoin\" VALUES (1, 'one', ST_GeomFromText('POLYGON ((-1.1 -1.1, -1.1 1.1, 1.1 1.1, 1.1 -1.1, -1.1 -1.1))', 4326), 1)");
+        run( "INSERT INTO \"ftjoin\" VALUES (2, 'two', ST_GeomFromText('POLYGON ((-10 -10, -10 10, 10 10, 10 -10, -10 -10))', 4326), 2)");
+        run( "INSERT INTO \"ftjoin\" VALUES (3, 'three', NULL, 3)");
+        
+        run( "CREATE TABLE \"ftjoin2\"(\"id\" int, \"join2intProperty\" int, \"stringProperty2\" varchar)");
+        run( "INSERT INTO \"ftjoin2\" VALUES (0, 0, '2nd zero')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (1, 1, '2nd one')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (2, 2, '2nd two')");
+        run( "INSERT INTO \"ftjoin2\" VALUES (3, 3, '2nd three')");
+        
     }
 
     @Override
     protected void dropJoinTable() throws Exception {
-        run( "DROP TABLE \"ftjoin\"" );
-        run( "DELETE FROM geometry_columns WHERE f_table_name = 'ftjoin'");
+        runSafe( "DROP TABLE \"ftjoin\"" );
+        runSafe( "DROP TABLE \"ftjoin2\"" );
+        runSafe( "DELETE FROM geometry_columns WHERE f_table_name = 'ftjoin'");
     }
 
 }